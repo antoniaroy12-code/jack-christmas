fac13a09a2af052e73650d5ac8203150a4c9dd87
antoniaroy12-code <antonia.roy12@gmail.com>
2025-12-25 09:47:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
-// Play Music
-const btn = document.getElementById("playBtn");
-const music = document.getElementById("bgMusic");
-btn.addEventListener("click", () => {
-  music.play();
-  btn.style.display = "none";
-});
+// Create Falling Snow ❄
+function createSnow() {
+  const snow = document.createElement("div");
+  snow.classList.add("snowflake");
+  snow.innerHTML = "❄";
 
-// Popup Zoom
-const popup = document.getElementById("popup");
-const popupImg = document.getElementById("popupImg");
+  snow.style.left = Math.random() * 100 + "vw";
+  snow.style.animationDuration = (Math.random() * 4 + 3) + "s";
+  snow.style.fontSize = (Math.random() * 14 + 10) + "px";
 
-document.querySelectorAll(".zoom").forEach(img => {
-  img.addEventListener("click", () => {
-    popupImg.src = img.src;
-    popup.style.display = "flex";
-  });
-});
+  document.querySelector(".snow-container").appendChild(snow);
 
-popup.addEventListener("click", () => popup.style.display = "none");
+  setTimeout(() => snow.remove(), 7000);
+}
+
+setInterval(createSnow, 120);
